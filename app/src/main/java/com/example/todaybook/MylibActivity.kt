92df6c810c5216d7cfb6315303bd835d4838aa4a
ybk2262 <ybk2262@naver.com>
2019-09-28 21:23:59 +0900

    .

--- app/src/main/java/com/example/todaybook/MylibActivity.kt
+++ app/src/main/java/com/example/todaybook/MylibActivity.kt
@@ -1,22 +1,46 @@
 package com.example.todaybook
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_mylib.*
+import kotlinx.android.synthetic.main.book_item.view.*
 
 class MylibActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mylib)
-        setSupportActionBar(toolbar)
+        supportActionBar?.title="검색제목"
+        val layoutManager = LinearLayoutManager(this@MylibActivity)
+        layoutManager.reverseLayout=true
+        layoutManager.stackFromEnd=true
+        recyclerView.layoutManager=layoutManager
+        recyclerView.adapter=MyAdapter()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    }
+    inner class MyViewHolder(book_item:View):RecyclerView.ViewHolder(book_item) {
+        val imageView:ImageView=book_item.imageView
+        val bookText:TextView=book_item.bookText
+    }
+    inner class MyAdapter:RecyclerView.Adapter<MyViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            return MyViewHolder(LayoutInflater.from(this@MylibActivity).inflate(R.layout.book_item,parent,false))
+        }
+
+        override fun getItemCount(): Int {
+            //return posts.size
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.bookText.text="몰라"
         }
     }
-
 }
