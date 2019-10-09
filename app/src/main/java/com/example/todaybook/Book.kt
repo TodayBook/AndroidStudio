2e20860113b81cfa8a142957d20d59d75d320975
aodlfdl2 <aodlfdl2@naver.com>
2019-10-09 21:55:35 +0900

    search_draft

---
+++ app/src/main/java/com/example/todaybook/Book.kt
@@ -0,0 +1,3 @@
+package com.example.todaybook
+
+class Book(val title:String, val author:String, val publisher:String, val image:String)
---
+++ app/src/main/java/com/example/todaybook/SearchListviewAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.todaybook
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class SearchListviewAdapter (val context: Context, val bookList: ArrayList<Book>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.search_listview_item, null)
+        val bookimage = view.findViewById<ImageView>(R.id.bookimageImg)
+        val booktitle = view.findViewById<TextView>(R.id.booktitleTv)
+        val bookpublisher = view.findViewById<TextView>(R.id.bookpublisherTv)
+        val bookauthor = view.findViewById<TextView>(R.id.bookauthorTv)
+
+
+        val book = bookList[position]
+        val resourceId = context.resources.getIdentifier(book.image, "drawable", context.packageName)
+        bookimage.setImageResource(resourceId)
+        booktitle.text = book.title
+        bookpublisher.text = book.publisher
+        bookauthor.text = book.author
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return bookList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return bookList.size
+    }
+
+
+}
