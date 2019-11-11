e8487c663cc11125eace43058d7bb6a5a4f55c7b
ybk2262 <ybk2262@naver.com>
2019-11-11 22:57:57 +0900

    .

--- app/src/main/java/com/example/todaybook/MylibActivity.kt
+++ app/src/main/java/com/example/todaybook/MylibActivity.kt
@@ -13,10 +13,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.FirebaseDatabase
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
 
 
 
@@ -27,6 +23,7 @@ class MylibActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mylib)
+
         var readList = ArrayList<ImageDataModel>()
         readList.clear()
 
@@ -52,6 +49,7 @@ class MylibActivity : AppCompatActivity() {
         var UserId:String
         if (cuser != null) {
             UserId=cuser.uid
+
             val readbooklistener = object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                     for (snapshot in dataSnapshot.children) {
--- app/src/main/java/com/example/todaybook/ReadOrRead.kt
+++ app/src/main/java/com/example/todaybook/ReadOrRead.kt
@@ -1,7 +1,9 @@
 package com.example.todaybook
 
+import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_read_or_read.*
@@ -10,17 +12,21 @@ class ReadOrRead : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     var database = FirebaseDatabase.getInstance().reference
     val cuser = FirebaseAuth.getInstance().currentUser
+    fun EncodeString(title:String):String {
+        return title.replace(".", " ")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_or_read)
         val bookinfo by lazy { intent.extras!!["Info"] as BookInfo }
+        println(bookinfo.title)
         bt_didbook.setOnClickListener {
-            database.child("users").child(cuser?.uid!!).child("didBook").child(bookinfo.title)
-                .setValue(bookDB(bookinfo.imageurl, bookinfo.author, bookinfo.pub))
+            database.child("users").child(cuser?.uid!!).child("didBook").child(EncodeString(bookinfo.title)).setValue(bookDB(bookinfo.imageurl, bookinfo.author, bookinfo.pub))
+            Toast.makeText(baseContext, "Success!!", Toast.LENGTH_SHORT).show()
         }
         bt_willbook.setOnClickListener {
-            database.child("users").child(cuser?.uid!!).child("willBook").child(bookinfo.title)
-                .setValue(bookDB(bookinfo.imageurl, bookinfo.author, bookinfo.pub))
+            database.child("users").child(cuser?.uid!!).child("willBook").child(bookinfo.title).setValue(bookDB(bookinfo.imageurl, bookinfo.author, bookinfo.pub))
+            Toast.makeText(baseContext, "Success!!", Toast.LENGTH_SHORT).show()
         }
     }
 }
