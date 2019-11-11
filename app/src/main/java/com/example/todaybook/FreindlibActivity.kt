3a97365f5b8aa59b709c300d1734fb2445f033e0
ybk2262 <ybk2262@naver.com>
2019-11-11 23:45:34 +0900

    후기부분

--- app/src/main/java/com/example/todaybook/FreindlibActivity.kt
+++ app/src/main/java/com/example/todaybook/FreindlibActivity.kt
@@ -2,12 +2,34 @@ package com.example.todaybook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_freindlib.*
 
 class FreindlibActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    var database = FirebaseDatabase.getInstance().reference
+    val cuser = FirebaseAuth.getInstance().currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_freindlib)
+        val namelistener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    var key : String = snapshot.key.toString()
+                    var value = snapshot.value.toString()
+                    nametext.text=value+"님의 친구"
+                    if(key=="UserId")break
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("FFFFFF", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        database.child("users").child(cuser!!.uid).addValueEventListener(namelistener)
     }
 }
--- app/src/main/java/com/example/todaybook/lib_detail.kt
+++ app/src/main/java/com/example/todaybook/lib_detail.kt
@@ -2,9 +2,14 @@ package com.example.todaybook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_lib_detail.*
 
 class lib_detail : AppCompatActivity() {
@@ -19,8 +24,27 @@ class lib_detail : AppCompatActivity() {
         authorView.text = bookinfo.author
         pubView.text = bookinfo.pub
         Glide.with(coverimage).load(bookinfo.imageurl).into(coverimage)
+
+        val commentslistener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    var key : String = snapshot.key.toString()
+                    var value = snapshot.value.toString()
+                    comments.setText(value)
+                    if(key=="comments"){
+                        break
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("FFFFFF", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        database.child("users").child(cuser!!.uid).child("didBook").child(bookinfo.title).addValueEventListener(commentslistener)
+
         bt_complete.setOnClickListener{
-            database.child("users").child(cuser?.uid!!).child("didBook").child(bookinfo.title).child("comments").setValue(comments.text)
+            database.child("users").child(cuser?.uid!!).child("didBook").child(bookinfo.title).child("comments").setValue(comments.text.toString())
+            Toast.makeText(baseContext, "Success!!", Toast.LENGTH_SHORT).show()
         }
     }
 }
