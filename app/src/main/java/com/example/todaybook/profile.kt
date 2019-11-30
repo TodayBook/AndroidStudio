0010f09b47975931e0a0509989b923cea4bb65d6
ybk2262 <ybk2262@naver.com>
2019-12-01 03:40:29 +0900

    .

--- app/src/main/java/com/example/todaybook/profile.kt
+++ app/src/main/java/com/example/todaybook/profile.kt
@@ -1,18 +1,12 @@
 package com.example.todaybook
 
-import android.Manifest
+
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -23,7 +17,12 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.storage.UploadTask
 import com.google.android.gms.tasks.OnSuccessListener
-import org.jetbrains.anko.toast
+import com.bumptech.glide.Glide
+
+
+
+
+
 
 
 class profile : AppCompatActivity() {
@@ -31,11 +30,16 @@ class profile : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private val GET_GALLERY_IMAGE = 200
     val storage = FirebaseStorage.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        setmyid()
+
         downloadInMemory()
+
         bt_profileimg_change.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
@@ -55,23 +59,37 @@ class profile : AppCompatActivity() {
 
     }
      private fun downloadInMemory() {
-        var ref = storage.getReference().child("profileimg.jpg")
-        var ONE_MEGABYTE: Long = 1024 * 1024
-        ref?.getBytes(ONE_MEGABYTE).addOnCompleteListener {
-            if (it.isSuccessful)
-                toast("이미지 다운로드 성공")
-            profile_img.setImageBitmap(byteArrayToBitmap(it.result!!))
-        }
+         val storageRef = storage.reference
+         var ref = storageRef.child("profileimg/"+auth.uid)
+         ref.getDownloadUrl().addOnSuccessListener(OnSuccessListener<Any> { uri ->
+             val imageURL = uri.toString()
+             Glide.with(applicationContext).load(imageURL).into(profile_img)
+         }).addOnFailureListener(OnFailureListener {
+             print("download failed")
+         })
     }
 
-    private fun byteArrayToBitmap(byteArry: ByteArray): Bitmap {
-        var bitmap:Bitmap?=null
-        bitmap = BitmapFactory.decodeByteArray(byteArry,0,byteArry.size)
-        return bitmap
-    }
-
-    private fun changeId(){
+    private fun setmyid(){
         val myIdlistener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    var key: String = snapshot.key.toString()
+                    println(key)
+                    var value = snapshot.value.toString()
+                    if (key == "UserId") {
+                        println(value)
+                        change_id.setText(value)
+                        break
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+        database.child("users").child(auth.uid!!).addValueEventListener(myIdlistener)
+    }
+    private fun changeId(){
+        val myIdchangelistener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 for (snapshot in dataSnapshot.children) {
                     var key: String = snapshot.key.toString()
@@ -84,14 +102,14 @@ class profile : AppCompatActivity() {
             override fun onCancelled(databaseError: DatabaseError) {
             }
         }
-        database.child("users").child(auth.uid!!).addValueEventListener(myIdlistener)
+        database.child("users").child(auth.uid!!).addValueEventListener(myIdchangelistener)
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GET_GALLERY_IMAGE && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             val selectedImageUri = data.data
             profile_img.setImageURI(selectedImageUri)
-            val riversRef = storage.getReference().child("profileimg.jpg")
+            val riversRef = storage.getReference().child("profileimg/").child(auth.uid!!)
             riversRef.putFile(selectedImageUri!!)
                 .addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
                     Toast.makeText(baseContext, "upload!", Toast.LENGTH_SHORT).show()
